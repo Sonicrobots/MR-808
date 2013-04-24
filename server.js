843ba38ae390a5adbefaa8dc4b4d7b19648146bc
Karsten Gebbert <karsten@null2.net>
2013-04-24 22:24:09 +0200

    introducing express.js
    Signed-off-by: Karsten Gebbert <karsten@null2.net>

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var app = require('http').createServer(handler),
-    path = require('path'),
+/* global require, process */
+
+var path = require('path'),
     url = require('url'),
-    io = require('socket.io').listen(app),
-    fs = require('fs');
+    fs = require('fs'),
+    express = require('express'),
+    app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(3000);
 
 var clockCount = 0;
 
@@ -23,37 +29,15 @@ var pattern = {
   ]
 };
 
-app.listen(3000);
-
-// web server for static files
-function handler (request, response) {
-  var uri = url.parse(request.url).pathname,
-      filename = path.join(process.cwd(), uri);
-
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-};
+// root route
+app.get("/", function(request, response) {
+  response.sendfile(path.join(process.cwd(), "index.html"));
+});
+// static content served from here
+app.use("/img", express.static( path.join( process.cwd(), "img")));
+app.use("/css", express.static( path.join( process.cwd(), "css")));
+app.use("/fonts", express.static( path.join( process.cwd(), "fonts")));
+app.use("/js", express.static( path.join( process.cwd(), "js")));
 
 // handle client updates
 io.sockets.on('connection', function (socket) {
