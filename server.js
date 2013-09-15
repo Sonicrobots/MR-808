3b19e386d31e73f866d8f3853dc61fbc764f6756
Karsten Gebbert <karsten@null2.net>
2013-09-15 22:26:31 +0200

    initialization and reset of sequencer

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var path = require('path'),
     url = require('url'),
     fs = require('fs'),
+    lodash = require('lodash'),
     express = require('express'),
     app = require('express')(),
     cookieParser = express.cookieParser(),
@@ -11,7 +12,29 @@ var path = require('path'),
     osc = require('node-osc'),
     clockServer = new osc.Server(7771,"0.0.0.0"),
     updateClient = new osc.Client("0.0.0.0", 57120),
-    webServerPort;
+    webServerPort, lastChangeOccurred = new Date().getTime(),
+    RESET_PATTERN_TIMEOUT = 1000 * 60 * 2,
+    defaultPattern = {
+      tempo: 128,
+      tracks: [
+        { id: "0", name: "bd", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "1", name: "topsnare", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "2", name: "bottomsnare", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "3", name: "small-cong", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "4", name: "medium-cong", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "5", name: "large-cong", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "6", name: "claves", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "7", name: "tophats", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "8", name: "bottomhats", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "9", name: "clap", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "10", name: "carabassa", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "11", name: "cym", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
+        { id: "12", name: "cowbell", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
+      ]
+    };
+
+// global variable to store state
+var pattern = lodash.cloneDeep(defaultPattern);
 
 // set up the port on which the server will run
 if(process.env['SEQ_SERVER_PORT']) {
@@ -26,45 +49,8 @@ else {
   webServerPort = 3000;
 }
 
-// global variable to store state
-var pattern = {
-  tempo: 120,
-  tracks: [
-    { id: "0", name: "bd", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "1", name: "topsnare", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "2", name: "bottomsnare", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "3", name: "small-cong", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "4", name: "medium-cong", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "5", name: "large-cong", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "6", name: "claves", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "7", name: "tophats", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "8", name: "bottomhats", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "9", name: "clap", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "10", name: "carabassa", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "11", name: "cym", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] },
-    { id: "12", name: "cowbell", steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] }
-  ]
-};
-
-//  // socket for receiving clock pulses + current step
-// function initializeSongUpdateSocket(c) {
-//   // assign the connection varable for updates during callbacks
-//   songUpdateSocketConn = c;
-//   // write current state of pattern to socket once a connection is open
-//   c.write(JSON.stringify({ type: "init", data: pattern }) + "\r\n");
-// }
-
 // web server
 server.listen(webServerPort);
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.round(Math.random() * 15)];
-    }
-    return color;
-}
 
 // configuration for all environments
 app.configure(function() {
@@ -72,7 +58,6 @@ app.configure(function() {
   app.use(express.bodyParser());
   // parse Cookies
   app.use(cookieParser);
-
   // static content
   app.use("/img", express.static( path.join( process.cwd(), "img")));
   app.use("/css", express.static( path.join( process.cwd(), "css")));
@@ -118,8 +103,27 @@ writeSockets.on('connection', function(socket) {
 
 // socket for receiving clock pulses + current step
 clockServer.on('message', function(msg, rinfo) {
+  if(msg[0] === '/init') {
+    lodash.forEach(pattern.tracks, function(track) {
+      // put the state update into the right place
+      updateClient.send('/song_update',"(id: " + track.id + ", type: \"track\", data: " + JSON.stringify(track.steps) + ")");
+    });
+  }
+
   if(msg[0] === '/clock')
     readOnlySockets.emit("clock-event", { step: "step-" + msg[1].toString() });
+
+  if( new Date().getTime() - lastChangeOccurred > RESET_PATTERN_TIMEOUT ) {
+    pattern = lodash.cloneDeep(defaultPattern); //reset pattern
+    readOnlySockets.emit('initialize', pattern); //tell every client
+
+    lodash.forEach(pattern.tracks, function(track) {
+      // put the state update into the right place
+      updateClient.send('/song_update',"(id: " + track.id + ", type: \"track\", data: " + JSON.stringify(track.steps) + ")");
+    });
+
+    lastChangeOccurred = new Date().getTime();
+  }
 });
 
 process.on('uncaughtException', function(err) {
